08a432a60a1c6dbb7343b62265accc04bd92ee41
Igor Popach <igorpopach@gmail.com>
2019-12-26 20:38:20 +0200

    add jest, enzyme

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {
-    CleanWebpackPlugin
-} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const { isDevelopment } = require('./utils/modes');
+
+const pathToJS = path.join(__dirname, 'src', 'index.tsx');
+const pathToHTML = path.join(__dirname, 'public', 'index.html');
+const pathToBuild = path.join(__dirname, 'build');
 
 module.exports = {
     entry: {
-        index: './src/index.js',
+        index: pathToJS,
     },
+
+    // style of source mapping to enhance the debugging process
     devtool: 'inline-source-map',
+
     module: {
         rules: [
             {
@@ -39,14 +46,20 @@ module.exports = {
             {
                 test: /\.s[ac]ss$/i,
                 use: [
+
+                    // Inject CSS into the DOM
                     'style-loader',
                     {
                         loader: MiniCssExtractPlugin.loader,
                         options: {
-                            hmr: process.env.NODE_ENV === 'development',
+                            hmr: isDevelopment(),
                         },
                     },
+
+                    // The css-loader interprets @import and url() like import/require() and will resolve them
                     'css-loader',
+
+                    // Loads a Sass/SCSS file and compiles it to CSS
                     'sass-loader',
                 ],
             },
@@ -71,18 +84,28 @@ module.exports = {
     optimization: {
         nodeEnv: 'production',
         minimizer: [
+            // minify JavaScript
             new UglifyJsPlugin(),
+            // minify scc
             new OptimizeCSSAssetsPlugin({})
         ],
     },
     plugins: [
+
+        // interactive treemap visualization of the contents of all bundles
         new BundleAnalyzerPlugin({
             analyzerPort: process.env.VUE_CLI_MODERN_BUILD ? 8888 : 9999 // Prevents build errors when running --modern
         }),
+
+        // Cleaning up the /build folder
         new CleanWebpackPlugin(),
+
+        // simplifies creation of HTML files to serve your webpack bundles
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'public', 'index.html')
+            template: pathToHTML
         }),
+
+        // extracts CSS into separate files
         new MiniCssExtractPlugin({
             // Options similar to the same options in webpackOptions.output
             // all options are optional
@@ -90,9 +113,17 @@ module.exports = {
             chunkFilename: '[id].css',
             ignoreOrder: false, // Enable to remove warnings about conflicting order
         })
+        
     ],
+    devServer: {
+        contentBase: pathToBuild,
+        compress: true,
+        historyApiFallback: true,
+        port: 3000
+    },
     output: {
-        path: __dirname + '/build',
-        filename: '[name].bundle.js'
+        path: pathToBuild,
+        filename: '[name].bundle.js',
+        publicPath: '/',
     },
 };
