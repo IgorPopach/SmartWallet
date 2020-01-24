d82aafd96e32b0a50a2f2ced54d01372a91ad512
DrobenyukA <drobenyuk.a@gmail.com>
2020-01-24 08:55:32 +0200

    Prettified code

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,11 +1,11 @@
 import React, { FunctionComponent } from 'react';
-import { shallow } from "enzyme";
+import { shallow } from 'enzyme';
 
 import App, { AppProps } from './../components/App';
 
-it("<App />", () => {
+it('<App />', () => {
     const result = shallow(<App title="Ok" paragraph="Paragraph" />).contains(<h2>Ok</h2>);
     const result2 = shallow(<App title="Ok" paragraph="Paragraph" />).contains(<p>Paragraph</p>);
     expect(result).toBeTruthy();
     expect(result2).toBeTruthy();
-})
+});
