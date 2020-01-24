861cf84c4df3f480b97023046efaa588d42242bf
DrobenyukA <drobenyuk.a@gmail.com>
2020-01-24 09:22:28 +0200

    testing husky and lint-staged 2

--- src/store/session/actions.ts
+++ src/store/session/actions.ts
@@ -11,35 +11,40 @@ import {
     LoginFinished,
     LogoutStarts,
     LogoutFinished,
-    LOGOUT_STARTS,
 } from './types';
 import firebase from '../../firebase';
 import { User } from '../../types/User';
 import { AppState } from '..';
 
-type RegistrationEpic = (login: string, password: string) => ThunkAction<
-    Promise<{ isSuccessful: boolean }>, // What should action return 
+type RegistrationEpic = (
+    login: string,
+    password: string,
+) => ThunkAction<
+    Promise<{ isSuccessful: boolean }>, // What should action return
     AppState, // TODO: get Application state type;
     undefined, // extra arguments (from middleware)
     RegistrationStarts | RegistrationFinished // TODO: add error action
 >;
 
 type InitializeSessionEpic = () => ThunkAction<
-    void, 
+    void,
     AppState,
     undefined,
     InitializeSessionStarts | InitializeSessionFinished | StoreUser // TODO: add error action
 >;
 
-type LogInEpic = (login: string, password: string) => ThunkAction<
-    void, 
+type LogInEpic = (
+    login: string,
+    password: string,
+) => ThunkAction<
+    void,
     AppState,
     undefined,
     LoginStarts | LoginFinished | StoreUser // TODO: add error action
 >;
 
 type LogOutEpic = () => ThunkAction<
-    void, 
+    void,
     AppState,
     undefined,
     LogoutStarts | LogoutFinished | StoreUser // TODO: add error action
@@ -53,7 +58,7 @@ const LOGOUT_FINISHED_ACTION = { type: TYPES.LOGOUT_FINISHED };
 const REGISTRATION_STARTS_ACTION = { type: TYPES.REGISTRATION_STARTS };
 const REGISTRATION_FINISHED_ACTION = { type: TYPES.REGISTRATION_FINISHED };
 
-export const storeUser = (payload: User|null) => ({
+export const storeUser = (payload: User | null) => ({
     type: TYPES.STORE_USER,
     payload,
 });
@@ -63,37 +68,35 @@ function onSignIn(
     { displayName, uid, email, photoURL }: firebase.User,
 ) {
     console.log({ displayName, uid, email, photoURL });
-    dispatch(storeUser({ displayName, uid, email, photoURL,}));
+    dispatch(storeUser({ displayName, uid, email, photoURL }));
 }
 
 function onSignOut(
-    dispatch: ThunkDispatch<{}, undefined, InitializeSessionStarts | InitializeSessionFinished | StoreUser>
+    dispatch: ThunkDispatch<{}, undefined, InitializeSessionStarts | InitializeSessionFinished | StoreUser>,
 ) {
     dispatch(storeUser(null));
 }
 
 export const initializeSession: InitializeSessionEpic = () => (dispatch) => {
     dispatch(INITIALIZATION_STARTS_ACTION);
-    firebase
-        .auth()
-        .onAuthStateChanged((user) => {
-            if (user) {
-                onSignIn(dispatch, user);
-            } else {
-                onSignOut(dispatch);
-            }
-            dispatch(INITIALIZATION_FINISHED_ACTION);
-        });
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            onSignIn(dispatch, user);
+        } else {
+            onSignOut(dispatch);
+        }
+        dispatch(INITIALIZATION_FINISHED_ACTION);
+    });
 };
 
-export const logIn: LogInEpic = (email: string, password: string) => (
-    dispatch
-) => {
+export const logIn: LogInEpic = (email: string, password: string) => (dispatch) => {
     dispatch(LOGIN_STARTS_ACTION);
     firebase
         .auth()
         .signInWithEmailAndPassword(email, password)
-        .then(() => { dispatch(LOGIN_FINISHED_ACTION); })
+        .then(() => {
+            dispatch(LOGIN_FINISHED_ACTION);
+        })
         .catch(({ code, message, ...errorsRest }) => {
             console.log({ code, message, errorsRest });
             // TODO: display error in Global errors
@@ -117,8 +120,8 @@ export const logOut: LogOutEpic = () => (dispatch) => {
 };
 
 export const registerNewUser: RegistrationEpic = (login: string, password: string) => (
-    dispatch
-): Promise<{isSuccessful: boolean }> => {
+    dispatch,
+): Promise<{ isSuccessful: boolean }> => {
     dispatch(REGISTRATION_STARTS_ACTION);
     return firebase
         .auth()
